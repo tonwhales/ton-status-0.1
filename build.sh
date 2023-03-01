fadd3960239a82f7bde7f21c3f7379518bf1c996
l2k1 <yma.het@gmail.com>
2023-03-01 16:50:36 +0600

    Added source fetch step to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget "https://launchpad.net/~yma-het/+archive/ubuntu/ton/+sourcefiles/ton-status/0.1-20/ton-status_0.1.orig.tar.xz" -O ../ton-status_0.1.orig.tar.xz
 rm -rf debian/patches/
 dpkg-source --commit
 TEMPDIR=$(mktemp -d)
