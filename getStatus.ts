b17cc3fc10d910e84e6866667c2b6cead5663897
yma.het <yma.het@gmail.com>
2023-08-20 23:48:02 +0600

    Fixed build

--- getStatus.ts
+++ getStatus.ts
@@ -20,7 +20,7 @@ type Config = {
     }>
 }
 
-const conf: Config = JSON.parse(fs.readFileSync('./config.json.new', 'utf-8'));
+const conf: Config = JSON.parse(fs.readFileSync('/etc/ton-status/config.json.new', 'utf-8'));
 const pools = conf.pools;
 
 interface Contracts {
--- local_build.sh
+++ local_build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-wget https://deb.nodesource.com/node_16.x/pool/main/n/nodejs/nodejs_16.14.2-deb-1nodesource1_amd64.deb
-ar x nodejs_16.14.2-deb-1nodesource1_amd64.deb
+set -e
+wget https://deb.nodesource.com/node_20.x/pool/main/n/nodejs/nodejs_20.5.1-deb-1nodesource1_amd64.deb
+ar x nodejs_20.5.1-deb-1nodesource1_amd64.deb
 unxz data.tar.xz
 tar -xvf data.tar
-rm control.tar.gz data.tar debian-binary nodejs_16.14.2-deb-1nodesource1_amd64.deb
-export PATH=$PATH:$(pwd)/usr/bin/
-npm install
-npm install typescript
-./node_modules/typescript/bin/tsc --esModuleInterop --target ES2019 --moduleResolution node --module commonjs getStatus.ts
-npm install pkg
-./node_modules/.bin/pkg --targets node16-linux-x64 --config package.json getStatus.js
+rm control.tar.gz data.tar debian-binary nodejs_20.5.1-deb-1nodesource1_amd64.deb
+export PATH=$(pwd)/usr/bin/:$PATH
+$(pwd)/usr/bin/npm install yarn
+YARN="$(pwd)/usr/bin/node $(pwd)/node_modules/.bin/yarn"
+$YARN install
+$YARN tsc --p tsconfig.json
+$YARN add pkg
+$YARN pkg --targets node18-linux-x64 --config package.json getStatus.js
 mv ton-status debian/ton-status_
 rm getStatus.js package-lock.json
 rm -rf node_modules/
