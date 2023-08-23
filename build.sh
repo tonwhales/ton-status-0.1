0537c7516954065d7a16fccf501df9d72bc2861f
root <yma.het@gmail.com>
2023-08-23 08:12:01 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget "https://launchpad.net/~yma-het/+archive/ubuntu/ton/+sourcefiles/ton-status/0.1-20/ton-status_0.1.orig.tar.xz" -O ../ton-status_0.1.orig.tar.xz
+PREV_VERSION=$(dpkg-parsechangelog -c2 | grep -A 100 "^Changes:" | tail -n 3 | grep -o "(.*)" | sed 's@(\|)@@g')
+wget "https://launchpad.net/~yma-het/+archive/ubuntu/ton/+sourcefiles/ton-status/$PREV_VERSION/ton-status_0.1.orig.tar.xz" -O ../ton-status_0.1.orig.tar.xz
 rm -rf debian/patches/
 dpkg-source --commit
 TEMPDIR=$(mktemp -d)
