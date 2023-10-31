35ebe436dac0803a1d1f39a21ddc87b5d5b2b4cc
yma.het <yma.het@gmail.com>
2023-10-31 21:48:50 +0600

    Updated nodejs version in build script

--- local_build.sh
+++ local_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-wget https://deb.nodesource.com/node_20.x/pool/main/n/nodejs/nodejs_20.5.1-deb-1nodesource1_amd64.deb
-ar x nodejs_20.5.1-deb-1nodesource1_amd64.deb
+wget https://deb.nodesource.com/node_20.x/pool/main/n/nodejs/nodejs_20.9.0-1nodesource1_amd64.deb
+ar x nodejs_20.9.0-1nodesource1_amd64.deb
 unxz data.tar.xz
 tar -xvf data.tar
-rm control.tar.gz data.tar debian-binary nodejs_20.5.1-deb-1nodesource1_amd64.deb
+rm control.tar.gz data.tar debian-binary nodejs_20.9.0-1nodesource1_amd64.deb
 export PATH=$(pwd)/usr/bin/:$PATH
 $(pwd)/usr/bin/npm install yarn
 YARN="$(pwd)/usr/bin/node $(pwd)/node_modules/.bin/yarn"
