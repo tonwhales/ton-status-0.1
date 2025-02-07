da903255849506400dcf2e5faa26fdd830104d2c
Yma Het <yma.het@gmail.com>
2025-02-07 18:11:40 +0600

    fix: Update nodejs and make axios non-compilable

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -6,15 +6,10 @@ echo "Imgage id: $IMAGE_ID"
 FILES="$(docker run --rm $IMAGE_ID ls .. | grep -v app)"
 DEB_NAME=$(echo "$FILES" | grep ".dsc")
 VERSION=$(basename $DEB_NAME .dsc)
-# echo "Making dir for version: $VERSION"
-# mkdir -p $VERSION
-# for FILE in $(echo "$FILES"); do
-#     echo "Copying $FILE"
-#     docker run --rm $IMAGE_ID cat ../$FILE > $VERSION/$FILE
-# done
 # This command not only checks connection but also starts agent
 gpg-connect-agent /bye
 # fixme: add correct selinux rule
 sudo setenforce 0
 PKEY=$(gpg --armor --export yma.het@gmail.com)
-docker run -it -v /run/user/$(id -u)/gnupg/S.gpg-agent:/root/.gnupg/S.gpg-agent:Z --rm $IMAGE_ID bash -c "echo \"$PKEY\" | gpg --import; echo 'use-agent' > /root/.gnupg/gpg.conf ; echo 'pinentry-mode loopback' >> /root/.gnupg/gpg.conf ; cd .. ; debsign "$VERSION"_source.changes; dput ppa:yma-het/ton "$VERSION"_source.changes"
+# !!!!!!!!!!!!!!! IN CHANHELOG NAM E MUST BE yam, not Yma Het
+docker run -it -v /run/user/$(id -u)/gnupg/S.gpg-agent:/root/.gnupg/S.gpg-agent:Z --rm $IMAGE_ID bash -c "echo \"$PKEY\" | gpg --import; echo 'use-agent' > /root/.gnupg/gpg.conf ; echo 'pinentry-mode loopback' >> /root/.gnupg/gpg.conf ; echo 'test' | gpg --sign --armour; cd .. ; debsign "$VERSION"_source.changes; dput ppa:yma-het/ton "$VERSION"_source.changes"
--- local_build.sh
+++ local_build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -ex
-wget https://deb.nodesource.com/node_20.x/pool/main/n/nodejs/nodejs_20.9.0-1nodesource1_amd64.deb
-ar x nodejs_20.9.0-1nodesource1_amd64.deb
+URL="https://deb.nodesource.com/node_22.x/pool/main/n/nodejs/nodejs_22.13.1-1nodesource1_amd64.deb"
+wget $URL
+ar x $(basename $URL)
 unxz data.tar.xz
 tar -xvf data.tar
-rm control.tar.xz data.tar debian-binary nodejs_20.9.0-1nodesource1_amd64.deb
+rm control.tar.xz data.tar debian-binary $(basename $URL)
 export PATH=$(pwd)/usr/bin/:$PATH
 $(pwd)/usr/bin/npm install yarn
 YARN="$(pwd)/usr/bin/node $(pwd)/node_modules/.bin/yarn"
 $YARN install
 $YARN tsc --p tsconfig.json
 $YARN add pkg
-$YARN pkg --targets node18-linux-x64 --config package.json getStatus.js
+$YARN pkg --targets latest-linux-x64 --config package.json getStatus.js
 mv ton-status debian/ton-status_
 rm getStatus.js package-lock.json
 rm -rf node_modules/
